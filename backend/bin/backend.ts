83e566225447d7e3e35504da1b27931a4e39b72e
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2024-01-03 08:57:40 -0500

    ci-cd

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -4,6 +4,12 @@ import * as cdk from 'aws-cdk-lib';
 import { BackendStack } from '../lib/backend-stack';
 import { FrontendStack } from '../lib/frontend-stack';
 
+
+const env_sg_aws_prod_ca_central_1 = {
+    account: '640111341785',
+    region: 'ca-central-1'
+}
+
 const app = new cdk.App();
 new BackendStack(app, 'BackendStack', {
 });
@@ -12,5 +18,6 @@ new FrontendStack(app, 'FrontendStack', {
     paramProjectName: 'similisnap',
     paramProjectEnv: 'prd',
     paramProjectId: '01',
-    paramHostedZoneId: 'Z09125231W8DILLQ8TFEH'
+    paramHostedZoneId: 'Z09125231W8DILLQ8TFEH',
+    env: env_sg_aws_prod_ca_central_1
 });
