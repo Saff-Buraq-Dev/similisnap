d88f52df76d6fa006b377abf5fcac5054551a62a
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2024-01-03 12:50:56 -0500

    ci-cd

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -19,5 +19,6 @@ new FrontendStack(app, 'FrontendStack', {
     paramProjectEnv: 'prd',
     paramProjectId: '01',
     paramHostedZoneId: 'Z09125231W8DILLQ8TFEH',
+    paramCertificateArn: 'arn:aws:acm:us-east-1:640111341785:certificate/3c75226f-0771-4264-889c-22daa4e7e4f8',
     env: env_sg_aws_prod_ca_central_1
 });
